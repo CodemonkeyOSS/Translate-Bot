b73d36635cf3fe857bdde1a2298230bf5dca2ecf
Alex Barbour <abarbour@tenable.com>
2020-10-06 14:00:45 -0400

    Remove old onMessage and let bots be translated

--- src/app.js
+++ src/app.js
@@ -39,43 +39,6 @@ client.on('ready', () => {
   logger.info(`Logged into server as ${client.user.tag}`)
 })
 
-// On Message
-client.on('message', async function(message) {
-
-  if (message.author.id === client.id) return
-
-  if (message.mentions.has(client.user)) {
-    if (message.author == 251883305362915328) {
-      message.reply(InsultCompliment.Compliment());
-    } else {
-      message.reply(InsultCompliment.Insult());
-    }
-  } else {   
-      if (twitterTranslator.doTwitterLinksExistInContent(message) && config.translation.twitter) {
-        twitterTranslator.handleMessage(logger, message);
-        // TODO: Improve
-        return
-      }
-      if (linkParser.containsAnyLink(message.content)) {
-        let updatedMsg = ''
-        for (i = 0; i < 12; i++) {
-          // Sleep before checking embeds
-          await sleep(500)
-          logger.debug("Embed checker in loop: "+i)
-          // Forcefully check for updated message from API
-          updatedMsg = await message.fetch(force=true).then( updatedMsg => { return updatedMsg })
-          if (updatedMsg.embeds.length > 0) {
-            logger.info("Finally got out of the loop") 
-            break
-          }
-        }
-        if (config.translation.anyEmbed) {
-          await embedTranslator.handleMessage(logger, updatedMsg);
-        }
-      }
-  }
-})
-
 // On Fiery Death, log and attempt another login
 client.on('disconnect', () => {
   logger.warn(`Terminal disconnect. Attempting reconnection.`)
@@ -89,7 +52,7 @@ client.login(process.env.DISCORD_TRANSLATE_TOKEN)
 client.on('message', async function(message) {
 
   // Ignore myself or another bot
-  if (message.author.id === client.id || message.author.bot) return
+  if (message.author.id === client.id) return
 
   if (message.mentions.has(client.user)) {
     insultOrComplimentCommand(message)
