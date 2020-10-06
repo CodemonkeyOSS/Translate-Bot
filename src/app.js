b46b180c7d16ce10ad222739d7bef1b3c24818f6
highpwner <>
2020-10-06 12:46:45 -0400

    Refactor this a little before the next featureset

--- src/app.js
+++ src/app.js
@@ -49,37 +49,45 @@ client.login(process.env.DISCORD_TRANSLATE_TOKEN)
 // On Message
 client.on('message', async function(message) {
 
-  if (message.author.id === client.id) return
+  // Ignore myself or another bot
+  if (message.author.id === client.id || message.author.bot) return
 
   if (message.mentions.has(client.user)) {
-    if (message.author == 251883305362915328) {
-      message.reply(InsultCompliment.Compliment());
-    } else {
-      message.reply(InsultCompliment.Insult());
-    }
-  } else {   
-      if (twitterTranslator.doTwitterLinksExistInContent(message) && config.translation.twitter) {
-        twitterTranslator.handleMessage(logger, message);
-      }
-      if (telegramTranslator.doTelegramLinksExistInContent(message)) {
-        let updatedMsg = ''
-        for (i = 0; i < 12; i++) {
-          // Sleep before checking embeds
-          await sleep(500)
-          logger.debug("Embed checker in loop: "+i)
-          // Forcefully check for updated message from API
-          updatedMsg = await message.fetch(force=true).then( updatedMsg => { return updatedMsg })
-          if (updatedMsg.embeds.length > 0) {
-            logger.info("Finally got out of the loop") 
-            break
-          }
-        }
-        if (config.translation.telegram) {
-          await telegramTranslator.handleMessage(logger, updatedMsg);
-        }
-      }
+    insultOrComplimentCommand(message)
+  } else {
+    processMessageTranslations(message)
   }
 })
 
+function insultOrComplimentCommand(message) {
+  if (message.author == 251883305362915328) {
+    message.reply(InsultCompliment.Compliment());
+  } else {
+    message.reply(InsultCompliment.Insult());
+  }
+}
+
+function processMessageTranslations(message) {
+  if (twitterTranslator.doTwitterLinksExistInContent(message) && config.translation.twitter) {
+    twitterTranslator.handleMessage(logger, message);
+  }
+  if (telegramTranslator.doTelegramLinksExistInContent(message)) {
+    let updatedMsg = ''
+    for (i = 0; i < 12; i++) {
+      // Sleep before checking embeds
+      await sleep(500)
+      logger.debug("Embed checker in loop: "+i)
+      // Forcefully check for updated message from API
+      updatedMsg = await message.fetch(force=true).then( updatedMsg => { return updatedMsg })
+      if (updatedMsg.embeds.length > 0) {
+        logger.info("Finally got out of the loop") 
+        break
+      }
+    }
+    if (config.translation.telegram) {
+      await telegramTranslator.handleMessage(logger, updatedMsg);
+    }
+  }
+}
 
 
