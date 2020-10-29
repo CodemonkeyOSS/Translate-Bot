8ef7f3fdb1fee604ecaa3625518655f91ed72b09
Alex Barbour <abarbour@tenable.com>
2020-10-29 12:57:48 -0400

    Use Google Cloud for detections

--- src/translators/detection.js
+++ src/translators/detection.js
@@ -1,16 +1,22 @@
-const translate = require('@vitalets/google-translate-api');
+const {Translate} = require('@google-cloud/translate').v2;
+const { Message } = require('discord.js');
 var langdetect = require('langdetect');
+
+const projectId = '121843099390';
+
+const translate = new Translate({projectId});
 
 function isMaybeEnglish(text) {
     return langdetect.detectOne(text) == 'en'
 }
 
 async function detectLanguage(text) {
-    let res = await translate(text, {to: 'en'})
-    if ( res.from.language.iso == 'iw') {
+    let res = await translate.detect(text)
+    let possibleLang = res[0].language
+    if ( possibleLang == 'iw') {
         return 'he'
     } else {
-        return res.from.language.iso
+        return possibleLang
     }
 }
 
