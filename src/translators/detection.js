3b7f771c8fab78145143abbe7f38fafeaf85f95c
Alex Barbour <abarbour@tenable.com>
2020-10-29 12:59:47 -0400

    Revert "Use Google Cloud for detections"
    This reverts commit 8ef7f3fdb1fee604ecaa3625518655f91ed72b09.

--- src/translators/detection.js
+++ src/translators/detection.js
@@ -1,22 +1,16 @@
-const {Translate} = require('@google-cloud/translate').v2;
-const { Message } = require('discord.js');
+const translate = require('@vitalets/google-translate-api');
 var langdetect = require('langdetect');
-
-const projectId = '121843099390';
-
-const translate = new Translate({projectId});
 
 function isMaybeEnglish(text) {
     return langdetect.detectOne(text) == 'en'
 }
 
 async function detectLanguage(text) {
-    let res = await translate.detect(text)
-    let possibleLang = res[0].language
-    if ( possibleLang == 'iw') {
+    let res = await translate(text, {to: 'en'})
+    if ( res.from.language.iso == 'iw') {
         return 'he'
     } else {
-        return possibleLang
+        return res.from.language.iso
     }
 }
 
