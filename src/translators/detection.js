f6f1992d67bb8f7284bebc50278b40a0cede06bd
Codemonkey <jalexbarbour@gmail.com>
2020-11-01 10:49:51 -0500

    Switch twitter to use newer translate package (#29)
    Co-authored-by: CodeMonkey <>

--- src/translators/detection.js
+++ src/translators/detection.js
@@ -1,29 +1,17 @@
-const {Translate} = require('@google-cloud/translate').v2;
-const {Message} = require('discord.js');
-var langdetect = require('langdetect');
-
-let options = {
-    projectId: '121843099390',
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_CLIENT_KEY
+class DetectionService {
+    constructor({ translate }) {
+        this.translate = translate;
     }
-}
-const translate = new Translate(options);
 
-function isMaybeEnglish(text) {
-    return langdetect.detectOne(text) == 'en'
-}
-
-async function detectLanguage(text) {
-    let res = await translate.detect(text)
-    let lang = res[0].language
-    if ( lang == 'iw') {
-        return 'he'
-    } else {
-        return lang
+    async detectLanguage(text) {
+        let res = await this.translate.detect(text)
+        let lang = res[0].language
+        if ( lang == 'iw') {
+            return 'he'
+        } else {
+            return lang
+        }
     }
 }
 
-exports.isMaybeEnligh = isMaybeEnglish;
-exports.detectLanguage = detectLanguage;
+module.exports = DetectionService;
