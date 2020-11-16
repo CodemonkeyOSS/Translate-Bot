803a5f63aa18594b08a4e532513b5ca6c1268f72
Alex Barbour <abarbour@tenable.com>
2020-11-16 08:56:09 -0500

    Ignore quoted links

--- src/translators/twitter.js
+++ src/translators/twitter.js
@@ -17,12 +17,14 @@ const twitter = new Twitter({
 /**
  * getDistinctTwitterLinksInContent is self explanatory. It literally checks a message and if it has a twitter link, it extracts it.
  * 
+ * NOTE: The regex is configured to ignore any line that starts with '>', so as to ignore quoted text and reduce translation cost and noise.
+ * 
  * Returns a set of potential matches
  * An empty set is akin to "no results"
  */
 //TODO Support multiple links by putting all links into an array
 function getDistinctTwitterLinksInContent(msgContent) {
-    var regex = /https:\/\/(?:www\.)?(?:mobile\.)?twitter\.com\/(?<handle>[a-zA-Z0-9_]+)\/status\/(?<status_id>[0-9]+)/g
+    var regex = /^(?<!\>.+)https:\/\/(?:www\.)?(?:mobile\.)?twitter\.com\/(?<handle>[a-zA-Z0-9_]+)\/status\/(?<status_id>[0-9]+).*$/gm
     let matches = []
     while((matchItem = regex.exec(msgContent)) != null) {
       matches.push(matchItem.groups)
@@ -46,6 +48,7 @@ function doTwitterLinksExistInContent(msg) {
  * Primary function, handles processing the message and sending back any translations on the original channel id
  */
 function handleMessage(logger, translate, message) {
+  logger.debug("text is:\n\n"+message.content+"\n\n")
   var twitterLinks = getDistinctTwitterLinksInContent(message.content)
   if (twitterLinks.length > 0) {
       twitterLinks.forEach( data => {
--- src/utils/link-parser.js
+++ src/utils/link-parser.js
@@ -7,7 +7,7 @@ function containsOnlyLink(text) {
       if ( matchItem[0] === text) return true
     }
   
-    var regexOnlyUrl = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/
+    var regexOnlyUrl = /^(?<!\>.+)((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/
     while((matchItem = regexOnlyUrl.exec(text)) != null) {
       if ( matchItem[0] === text ) return true
     }
