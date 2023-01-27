cb66279a02774f57f56956f3e605001aa732d3eb
codemonkeyoss <codemonkeyoss@gmail.com>
2023-01-27 00:15:40 -0500

    Refactor twitter translator for v2

--- src/translators/twitter.js
+++ src/translators/twitter.js
@@ -1,4 +1,5 @@
-var Twitter = require('twitter');
+const { TwitterApi } = require('twitter-api-v2');
+const util = require('util');
 var dateUtils = require('../utils/date-utils');
 var linkParser = require('../utils/link-parser');
 var iso6391 = require('iso-639-1');
@@ -8,11 +9,7 @@ const DetectionService = require('../services/detection');
 /**
  * Setup twitter client so we can talk to twitter API
  */
-const twitter = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_SECRET_KEY,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
-})
+const twitter = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
   
 /**
  * getDistinctTwitterLinksInContent is self explanatory. It literally checks a message and if it has a twitter link, it extracts it.
@@ -64,79 +61,97 @@ function handleMessage(logger, translate, message) {
   Meat and taters function
 */
 async function translateAndSend(logger, translate, message, data) {
-    twitter.get(`statuses/show.json?id=` + data.status_id, { tweet_mode:"extended"}, async function(error, tweets, response) {
-      if(error) {
-        logger.error("Error communicating with twitter: "+error);
-       } else {
-        
-        var jsonResponse = tweets
-  
-        if ( linkParser.containsOnlyLink(jsonResponse.full_text) ) {
-          logger.debug("Tweet contains only a link, ignoring.")
-          return
-        }
+  console.log(data.status_id)
 
-        const detectionService = new DetectionService(process.env.DL_KEY)
+  const response = await twitter.v2.singleTweet(data.status_id, {
+    'expansions': [
+      'author_id'
+    ],
+    'user.fields': [
+      'name',
+      'profile_image_url'
+    ],
+    'tweet.fields': [
+      'created_at',
+      'lang'
+    ]
+  });
+  //console.log(util.inspect(response, {depth: null}))
 
-        // Process language metadata and decide on source language
-        let possibleLang = null 
-        try {
-          if (detectionService.isMaybeEnglishOffline(jsonResponse.full_text)) {
-            possibleLang = 'en'
-          } else {
-            possibleLang = await detectionService.detectLanguage(jsonResponse.full_text)
-          }
-          logger.debug(`[TWITTER] Language is suspected to be: ${possibleLang}`)
-          
-          // und seems to mean the service couldn't quite figure itself out. We will black hole these along with ignoring english.
-          if (possibleLang == 'en' || possibleLang == 'und') {
-            return
-          }
-        } catch (e) {
-          if (e == "UNRELIABLE") {
-            message.reply({ content: `the language detection was unreliable so I can't do anything here. Please report it if you have concerns.`}).then(msg => {
-              msg.delete({timeout: 10000})
-            })
-            return
-          } else if (e == "NO_DETECTION") {
-            logger.warn("Translation service did not detect any language, assuming it was a no-op.")
-            return
-          }
-        }
-        
-        translate.translate(tweets.full_text, 'en').then(res => {
-          var translated = res[1].data.translations[0]
+  const tweetData = response.data
+  const userData = response.includes.users[0]
 
-          if (translated.detectedSourceLanguage == 'en') {
-            logger.error("GCT thinks this is already English, so we wasted a translation.")
-            return
-          }
+  const tweetText = tweetData.text
 
-          var language = iso6391.getName(translated.detectedSourceLanguage)
-          // If Google doesn't give us an discernible iso code (ex - iw is no longer used), fallback to what twitter might tell us.
-          if (language == "") {
-            language = iso6391.getName(possibleLang)
-          }
-
-          const embed = new EmbedBuilder()
-            .setColor(0x00afff)
-            .setAuthor({
-              name: jsonResponse.user.name + " (@" + jsonResponse.user.screen_name + ")",
-              iconURL: jsonResponse.user.profile_image_url,
-              url: "https://twitter.com/" + jsonResponse.user.screen_name + "/status/" + jsonResponse.id_str
-            })
-            .setDescription(translated.translatedText)
-            .addFields(
-              { name: "Published On", value: dateUtils.prettyPrintDate(jsonResponse.created_at) }
-            )
-            .setFooter({ text: 'Translated from '+language+' with love by CodeMonkey'})
-
-          message.reply({ embeds: [embed]})
-          logger.info('[TRANSLATION] server='+message.channel.guild.name+', source=twitter, srcLanguage='+language+', user='+jsonResponse.user.screen_name+', id='+jsonResponse.id_str)
-        })
-       }
-    })
+  if ( linkParser.containsOnlyLink(tweetText) ) {
+    logger.debug("Tweet contains only a link, ignoring.")
+    return
   }
+
+  const detectionService = new DetectionService(process.env.DL_KEY)
+
+  /**
+  // Process language metadata and decide on source language
+  let possibleLang = tweetData.lang
+  try {
+    if (detectionService.isMaybeEnglishOffline(tweetText)) {
+      possibleLang = 'en'
+    } else {
+      possibleLang = await detectionService.detectLanguage(tweetText)
+    }
+    logger.debug(`[TWITTER] Language is suspected to be: ${possibleLang}`)
+    
+    // und seems to mean the service couldn't quite figure itself out. We will black hole these along with ignoring english.
+    if (possibleLang == 'en' || possibleLang == 'und') {
+      return
+    }
+  } catch (e) {
+    if (e == "UNRELIABLE") {
+      message.reply({ content: `the language detection was unreliable so I can't do anything here. Please report it if you have concerns.`}).then(msg => {
+        msg.delete({timeout: 10000})
+      })
+      return
+    } else if (e == "NO_DETECTION") {
+      logger.warn("Translation service did not detect any language, assuming it was a no-op.")
+      return
+    }
+  }
+  */
+
+  // WIP - New guard clause to remove english tweets
+  if (tweetData.lang == 'en') { return }
+
+  translate.translate(tweetText, 'en').then(res => {
+    var translated = res[1].data.translations[0]
+
+    if (translated.detectedSourceLanguage == 'en') {
+      logger.error("GCT thinks this is already English, so we wasted a translation.")
+      return
+    }
+
+    var language = iso6391.getName(translated.detectedSourceLanguage)
+    // If Google doesn't give us an discernible iso code (ex - iw is no longer used), fallback to what twitter might tell us.
+    if (language == "") {
+      language = iso6391.getName(possibleLang)
+    }
+
+    const embed = new EmbedBuilder()
+      .setColor(0x00afff)
+      .setAuthor({
+        name: userData.name + " (@" + userData.username + ")",
+        iconURL: userData.profile_image_url,
+        url: "https://twitter.com/" + userData.username + "/status/" + tweetData.id
+      })
+      .setDescription(translated.translatedText)
+      .addFields(
+        { name: "Published On", value: dateUtils.prettyPrintDate(tweetData.created_at) }
+      )
+      .setFooter({ text: 'Translated from '+language+' with love by CodeMonkey'})
+
+    message.reply({ embeds: [embed]})
+    logger.info('[TRANSLATION] server='+message.channel.guild.name+', source=twitter, srcLanguage='+language+', user='+userData.username+', id='+tweetData.id)
+  })
+}
 
 exports.doTwitterLinksExistInContent = doTwitterLinksExistInContent;
 exports.getDistinctTwitterLinksInContent = getDistinctTwitterLinksInContent;
